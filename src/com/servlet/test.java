2ab01765b4223314e6be159a5d2d4976c726b72b
YiWuJu <yiwuji@139.com>
2024-03-23 22:03:29 +0800

    create servlet 'test' with project 'tt'

---
+++ src/com/servlet/test.java
@@ -0,0 +1,24 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain;charset=UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello");
+        writer.write("a + b = " + req.getParameter("a") + req.getParameter("b"));
+    }
+}
